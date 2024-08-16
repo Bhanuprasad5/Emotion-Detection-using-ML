80727e2b20a594446a7dd1ad5a5090b2929d08a5
Bhanu prasad Chouki <129620298+Bhanuprasad5@users.noreply.github.com>
2024-08-17 00:06:48 +0530

    emotiondetection.py

--- emotiondetection.py
+++ emotiondetection.py
@@ -9,10 +9,10 @@ import streamlit as st
 
 
 
-image= Image.open(r"C:\Users\chouk\Streamlit\573326-innomatics_research_labs_logo.png")
+image= Image.open(r"573326-innomatics_research_labs_logo.png")
 st.image(image)
 # st.image(r"573326-innomatics_research_labs_logo.png")
-model = pickle.load(open(r"C:\Users\chouk\Streamlit\bhanu\emotion_detection.pkl","rb"))
+model = pickle.load(open(r"emotion_detection.pkl","rb"))
 
 text = st.text_input("Enter the Text :")
 
@@ -25,4 +25,4 @@ if st.button("Submit"):
     
     pred = model.predict([text])[0]
     st.write("The Email is : ",pred)
-    st.image(r"C:\Users\chouk\Streamlit\bhanu\joy emoji.jpeg",width=200)
+    st.image(r"joy emoji.jpeg",width=200)
